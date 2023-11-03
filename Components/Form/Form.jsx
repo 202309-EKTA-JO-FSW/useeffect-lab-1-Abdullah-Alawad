c5f82f9b1f682844997bdf84ca2900f614ecee8d
Abdullah Alawad <abdullahalawad123321@gmail.com>
2023-11-03 19:43:14 +0300

    Tests are Done

--- Components/Form/Form.jsx
+++ Components/Form/Form.jsx
@@ -23,7 +23,7 @@ export default function Form({ setNumberOfDogs }) {
         <button
           onClick={(e) => {
             e.preventDefault();
-            // When we click on the WOOF! button, what happens?
+            setNumberOfDogs(inputText);
           }}
           type="submit"
           className="btn btn-primary"
